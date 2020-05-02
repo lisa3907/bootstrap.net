bcc71f3c8e3b64d524eef0115c5f42e0a595a61b
odinsoft <odinsoft@odinsoftware.co.kr>
2020-05-03 02:02:38 +0900

    ..

--- src/BootStrap.Net/Util/EnumExtensions.cs
+++ src/BootStrap.Net/Util/EnumExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 
 namespace BootStrap.Net.Util
 {
@@ -7,37 +8,37 @@ namespace BootStrap.Net.Util
         public static string ToDescriptionString(this Color val)
         {
             DescriptionAttribute[] attributes = (DescriptionAttribute[])val.GetType().GetField(val.ToString()).GetCustomAttributes(typeof(DescriptionAttribute), false);
-            return attributes.Length > 0 ? attributes[0].Description : string.Empty;
+            return attributes.Length > 0 ? attributes[0].Description : String.Empty;
         }
 
         public static string ToDescriptionString(this Size val)
         {
             DescriptionAttribute[] attributes = (DescriptionAttribute[])val.GetType().GetField(val.ToString()).GetCustomAttributes(typeof(DescriptionAttribute), false);
-            return attributes.Length > 0 ? attributes[0].Description : string.Empty;
+            return attributes.Length > 0 ? attributes[0].Description : String.Empty;
         }
 
         public static string ToDescriptionString(this InputType val)
         {
             DescriptionAttribute[] attributes = (DescriptionAttribute[])val.GetType().GetField(val.ToString()).GetCustomAttributes(typeof(DescriptionAttribute), false);
-            return attributes.Length > 0 ? attributes[0].Description : string.Empty;
+            return attributes.Length > 0 ? attributes[0].Description : String.Empty;
         }
 
         public static string ToDescriptionString(this DropdownDirection val)
         {
             DescriptionAttribute[] attributes = (DescriptionAttribute[])val.GetType().GetField(val.ToString()).GetCustomAttributes(typeof(DescriptionAttribute), false);
-            return attributes.Length > 0 ? attributes[0].Description : string.Empty;
+            return attributes.Length > 0 ? attributes[0].Description : String.Empty;
         }
 
         public static string ToDescriptionString(this Placement val)
         {
             DescriptionAttribute[] attributes = (DescriptionAttribute[])val.GetType().GetField(val.ToString()).GetCustomAttributes(typeof(DescriptionAttribute), false);
-            return attributes.Length > 0 ? attributes[0].Description : string.Empty;
+            return attributes.Length > 0 ? attributes[0].Description : String.Empty;
         }
 
         public static string ToDescriptionString(this HeadingSize val)
         {
             DescriptionAttribute[] attributes = (DescriptionAttribute[])val.GetType().GetField(val.ToString()).GetCustomAttributes(typeof(DescriptionAttribute), false);
-            return attributes.Length > 0 ? attributes[0].Description : string.Empty;
+            return attributes.Length > 0 ? attributes[0].Description : String.Empty;
         }
     }
 }
